0531cdafd9c5de0ffe40b1194d3641c3f30186d2
OliverLemke <30410271+OliverLemke@users.noreply.github.com>
2017-09-20 15:06:17 +0200

    Add files via upload
    Fast Algorithm

--- Algorithms/Software/Algorithms_large/CNN_yes.py
+++ Algorithms/Software/Algorithms_large/CNN_yes.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 9 2015
-
-@author: Oliver Lemke
-"""
-
 # Stop loading time I
 
 startl=time.clock()
 
-print ('Loading distance matrix')
+print 'Loading distance matrix'
 
 # Import Python RMSD Matrix including Error report
 
@@ -22,42 +15,16 @@ N=np.int_(np.shape(dist)[0])
 
 # Prepare neighborlist
 
-Numbi=np.zeros((N))
-distnll=[0]*N
+Numbi=np.zeros(N)
+distn=[0]*N
 
-# Sort matrix according to the distance measurement
+# Calculate Neighborlist
 
-for i in range(0,N):
-    distsort=np.zeros((1,N,2))          
-    distsort[0,:,0]=np.argsort(dist[i,:])+1          
-    distsort[0,:,1]=np.sort(dist[i,:]) 
+for i in range(N):
 
-# Determine all Neighbors that are located within rc
-
-    j=0
-    Vecl=np.zeros((1,N))
-    Vecr=np.zeros((1,N))
-    while distsort[0,j,1]<=rc:
-        Vecl[0,j]=distsort[0,j,0]
-        j=j+1
-        Numbi[i]=Numbi[i]+1
-        if j==N:
-            break
-    nll=np.nonzero(Vecl)
-    distnll[i]=Vecl[0,nll[1]]
-
-# Calculate the total number of nearest neighbors
-
-km=int(max(Numbi))                  # Maximal number of nearest neighbors
-distn=np.zeros((N,km))              # Distance Matrix for nearest neighbors
-
-for i in range(0,N):
-    distn[i,0:len(distnll[i])]=distnll[i]           # Distance Matrix for nearest neighbors
-
-# Clear Memory II
-
-del distsort
-del distnll
+    Vec=np.nonzero(dist[i,:]<=rc)[0]
+    Numbi[i]=len(Vec)
+    distn[i]=Vec   
 
 # Preset parameters for the clustering
             
@@ -67,7 +34,7 @@ Clust=[0]*N         # Preset Cluster List
 
 # Filter noise points
 
-Numbi[np.nonzero(Numbi<nnnc)]=0
+Numbi[Numbi<nnnc]=0
 
 # Stop loading time II
 
@@ -79,25 +46,25 @@ start=time.clock()
 
 # Clustering
 
-print ('Clustering process started')
+print 'Clustering process started'
 
 while w<1:    
     
     # Find maximal number of nearest neighbors
     
-    Nmax=np.asarray(np.nonzero(Numbi==max(Numbi)))+1
+    Nmax=np.nonzero(Numbi==max(Numbi))[0]
     
     # Reset cluster
     
-    C=np.zeros((1,N),dtype=int)                 
+    C=np.zeros(N,dtype=int)                 
     
     # Write point with the highest density in the cluster
     
-    C[0,0]=Nmax[0,0]
+    C[0]=Nmax[0]
     
     # Cluster index
     
-    cl=np.int_(1)               
+    cl=int(1)               
     
     # Control value II
     
@@ -105,54 +72,38 @@ while w<1:
     
     # Cluster index for new added points    
     
-    lv=1                                    
-    ci=np.int_(np.zeros((1,N+1)))         
+    lv=0                                    
+    ci=np.zeros(N,dtype=int)      
     
     # Mask point with the highest density
     
-    Numbi[Nmax[0,0]-1]=0  
+    Numbi[Nmax[0]]=0  
 
     while cc>0:
 
         # Reset control value II and define new limits
         
-        ci[0,lv]=cl                   
+        ci[lv]=cl                   
         cc=0           
         
-        for a in range (ci[0,lv-1],ci[0,lv]):
+        for a in C[ci[lv-1]:ci[lv]]:
             
-            # Extract Neighborlist of a within rc             
-            
-            na=np.nonzero(distn[C[0,a]-1,:])
+            # Extract Neighborlist of a within rc
 
-            # Extract all reachable neighbors (rc)              
-            
-            Nr=distn[C[0,a]-1,na]                       
-            sNr=np.shape(Nr)[1]
+	    Na=distn[a]             
             
             # Compare Neighborlists of a and all reachable datapoints              
             
-            for c in range(0,sNr):
-                b=np.int_(Nr[0,c])
+            for b in Na:
                 
                 # Check if point is already clustered                  
                 
-                if Numbi[(b-1)]>0:
-                    nb=np.nonzero(distn[b-1,:])
+                if Numbi[b]>0:
+
+		    Nb=distn[b]
                     
-                    # List of nearest neighbors of a (Clustered) 
-                    
-                    Na=distn[C[0,a]-1,na]
-                   
-                    # List of nearest neighbors of b (Not clustered) 
-                    
-                    Nb=distn[b-1,nb]
-                    
-                    # Compare neighbors of a and b
-                    
-                    ttc=ismember(Na[0,:],Nb[0,:])
-                    tcl=np.asarray(ttc)+1
-                    tc=np.shape(np.nonzero(tcl))[1]
+                    tcc=np.asarray(ismember(Na,Nb))
+		    tc=len(tcc[tcc>=0])
                     
                     # Check if b in the Nearest Neighbors of a                    
                     
@@ -163,7 +114,7 @@ while w<1:
                     
                     # Check if a in the Nearest Neighbors of b
                     
-                    if C[0,a] in Nb:
+                    if a in Nb:
                         ta=1
                     else:
                         ta=0
@@ -174,13 +125,13 @@ while w<1:
                         
                         # Add point to the cluster
                         
-                        C[0,cl]=b
-                        cl=cl+1;
+                        C[cl]=b
+                        cl=cl+1
                         cc=cc+1
                         
                         # Mask clustered point
                         
-                        Numbi[b-1]=0
+                        Numbi[b]=0
                         
         # Update lv
                         
@@ -188,13 +139,9 @@ while w<1:
                         
     # Write Cluster to the Cluster List
                         
-    Cc=np.int_(C[np.nonzero(C)])                                    
+    Cc=np.int_(C[:cl])                                    
     Clust[cn]=Cc
-    cn=cn+1
-    
-    # Reset
-    
-    dd=0             
+    cn=cn+1           
     
     # Truncation criteria 
 
